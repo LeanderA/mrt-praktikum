0ea47e96848f465c35d06264edf16124e6de98bf
Praktikum MRT2 <nobody@tu-dresden.de>
2023-06-08 19:07:00 +0200

    5.6 Fertig

--- eet4_5.3/eet4_5.3.ino
+++ eet4_5.3/eet4_5.3.ino
@@ -1,6 +1,12 @@
 int delaytime;
+int maxPhase;
+int minPhase;
+int maxDelay;
+int minDelay;
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(0, INPUT);
   pinMode(1, INPUT);
   pinMode(6, INPUT);
@@ -8,23 +14,31 @@ void setup() {
   pinMode(3, OUTPUT);
   
   delaytime = 5000;
+  maxPhase = 160; // maximaler Winkel in Grad
+  minPhase = 5; // minimaler Winkel in Grad
+
+  maxDelay = 10000 / 180 * maxPhase;
+  minDelay = 10000 / 180 * minPhase;
 }
 
 
 void loop() {
   if(digitalRead(0) == HIGH) delaytime += 10;
   if(digitalRead(1) == HIGH) delaytime -= 10;
+  
+  if(delaytime > maxDelay) delaytime = maxDelay;
+  if(delaytime < minDelay) delaytime = minDelay;
 
   while(digitalRead(6) == LOW) {
     delayMicroseconds(1);
   }
-
   delayMicroseconds(delaytime);
   digitalWrite(3, HIGH);
   delayMicroseconds(10);
   digitalWrite(3, LOW);
-
   while(digitalRead(6) == HIGH) {
     delayMicroseconds(1);
   }
+
+  Serial.println(delaytime);
 }
