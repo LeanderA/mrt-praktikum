f7bf1a481738891e83c8233ad0d85b46c9d98064
Praktikum MRT2 <nobody@tu-dresden.de>
2023-06-08 18:58:31 +0200

    5.5 Fertig

--- eet4_5.3/eet4_5.3.ino
+++ eet4_5.3/eet4_5.3.ino
@@ -1,19 +1,25 @@
 int delaytime;
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
   pinMode(6, INPUT);
+
   pinMode(3, OUTPUT);
-  delaytime = 7000;
+  
+  delaytime = 5000;
 }
 
+
 void loop() {
+  if(digitalRead(0) == HIGH) delaytime += 10;
+  if(digitalRead(1) == HIGH) delaytime -= 10;
+
   while(digitalRead(6) == LOW) {
     delayMicroseconds(1);
   }
 
   delayMicroseconds(delaytime);
-
   digitalWrite(3, HIGH);
   delayMicroseconds(10);
   digitalWrite(3, LOW);
