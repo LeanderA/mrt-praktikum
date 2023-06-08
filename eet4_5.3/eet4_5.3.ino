2d6b27105480f8807c7648b8699a198750fed478
Praktikum MRT2 <nobody@tu-dresden.de>
2023-06-08 18:42:59 +0200

    5.4 Fertig

--- eet4_5.3/eet4_5.3.ino
+++ eet4_5.3/eet4_5.3.ino
@@ -1,21 +1,23 @@
-int count;
+int delaytime;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(6, INPUT);
   pinMode(3, OUTPUT);
-  count = 0;
+  delaytime = 7000;
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(3, LOW);
   while(digitalRead(6) == LOW) {
     delayMicroseconds(1);
   }
+
+  delayMicroseconds(delaytime);
+
   digitalWrite(3, HIGH);
-  count++;
-  Serial.println(count);
+  delayMicroseconds(10);
+  digitalWrite(3, LOW);
+
   while(digitalRead(6) == HIGH) {
     delayMicroseconds(1);
   }
